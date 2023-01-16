3196e272b8aeb82f662fb8f9f138e08ae81f594c
dilshanbeligala <beligaladilshan@gmail.com>
2023-01-16 14:58:58 +0530

    Fix vehical owner login

--- api/routes/AuthRoutes.js
+++ api/routes/AuthRoutes.js
@@ -5,8 +5,8 @@ module.exports = function(app){
     const AuthController = require("../controllers/AuthController");
 
     app.post("/register_vehicleowner", AuthController.registerVehicleOwner);
-    // app.post("/login",AuthController.login);
-    app.get("/vehicleowner", AuthController.getVehicleOwnerDetails);
+    app.post("/login_vehicleowner",AuthController.loginVehicleOwner);
+    // app.get("/vehicleowner", AuthController.getVehicleOwnerDetails);
 
     app.post("/register_shedowner",AuthController.registerShedOwner);
     app.post("/login_shedowner",AuthController.loginShedOwner);
